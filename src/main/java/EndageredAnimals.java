fcbb58805c98555b642e793957148cff334eb341
Keith Omware <keithomware@gmail.com>
2019-11-23 11:10:19 +0300

    Add an Endagered animal constructor in EndageredAnimals class

--- src/main/java/EndageredAnimals.java
+++ src/main/java/EndageredAnimals.java
@@ -1,11 +1,26 @@
-public class EndageredAnimals {
+import org.sql2o.Connection;
+
+import java.util.List;
+
+public class EndageredAnimals extends Animal {
+    private int id;
     private String name;
     private String health;
     private int age;
+
     public EndageredAnimals(String name,String health, int age) {
-        this.name = name;
+        super(name);
         this.health = health;
         this.age = age;
 
+    }
+    public String getHealth() {
+        return health;
+    }
+    public int getAge() {
+        return age;
+    }
+    public int getId() {
+        return id;
     }
 }
