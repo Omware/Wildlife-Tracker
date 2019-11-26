cb7d9cdccbc7306fcfbcf249e19d7680cca3ecc3
Keith Omware <keithomware@gmail.com>
2019-11-26 08:04:20 +0300

    Add username in DB.java class

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -18,7 +18,7 @@ public class DB {
             int port = dbUri.getPort();
             String host = dbUri.getHost();
             String path = dbUri.getPath();
-            String username = (dbUri.getUserInfo() == null) ? "wildlife-tracker" : dbUri.getUserInfo().split(":")[0];
+            String username = (dbUri.getUserInfo() == null) ? "keith" : dbUri.getUserInfo().split(":")[0];
             String password = (dbUri.getUserInfo() == null) ? "1234" : dbUri.getUserInfo().split(":")[1];
             sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, password);
 
